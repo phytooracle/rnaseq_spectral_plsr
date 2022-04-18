88183d420c7a3e304d44d13df450589edb448aa2
Emmanuel Gonzalez <emmanuelgonzalez@email.arizona.edu>
2022-04-18 13:46:49 -0700

    clean and doc

--- rnaseq_spectral_plsr.py
+++ rnaseq_spectral_plsr.py
@@ -6,27 +6,18 @@ Purpose: Partial least squares regression (PLSR) on gene expression (response va
 """
 
 import argparse
-# from math import perm
 import os 
 import sys
 from sys import stdout
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import multiprocessing
 from scipy.signal import savgol_filter
-from sklearn.preprocessing import StandardScaler, MinMaxScaler, MaxAbsScaler, RobustScaler
 from sklearn.cross_decomposition import PLSRegression
 from sklearn.model_selection import cross_val_predict
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.utils import shuffle
-from sklearn.model_selection import train_test_split, StratifiedKFold, permutation_test_score
+from sklearn.model_selection import train_test_split
 import matplotlib.collections as collections
 import pickle
-import multiprocessing
-import itertools
-import warnings
-warnings.filterwarnings("ignore")
 
 
 # --------------------------------------------------
@@ -91,6 +82,21 @@ def get_args():
 
 # --------------------------------------------------
 def pls_variable_selection(X, y, max_comp, transcript):
+    '''
+    Runs PLS variable selection given the explanatory and response variables, number of maximum components, and transcript name.
+
+    Input: 
+        - X: explanatory variable (hyperspectral data)
+        - y: response variable (RNA-Seq data)
+        - max_comp: maximum number of allowed components
+        - transcript: name of transcript, string variable
+
+    Output:
+        - Xc[:,mseminy[0]:]: explanatory variable filtered by number of components found in lowest MSE search 
+        - mseminx[0]+1: occurence of minimum MSE in x
+        - mseminy[0]: occurence of minimum MSE in y
+        - sorted_ind: index of spectra
+    '''
     
     # Define MSE array to be populated
     mse = np.zeros((max_comp,X.shape[1]))
@@ -131,57 +137,33 @@ def pls_variable_selection(X, y, max_comp, transcript):
     print("Wavelengths to be discarded ",mseminy[0])
     print('Optimized MSEP ', mse[mseminx,mseminy][0])
     stdout.write("\n")
-    # plt.imshow(mse, interpolation=None)
-    # plt.savefig(os.path.join(plot_out_dir, f'{transcript}_mse_plot.png'), transparent=True)
-    # plt.show()
+    plt.imshow(mse, interpolation=None)
+    plt.savefig(os.path.join(plot_out_dir, f'{transcript}_mse_plot.png'), transparent=True)
  
     # Calculate PLS with optimal components and export values
     pls = PLSRegression(n_components=mseminx[0]+1)
     pls.fit(X, y)
         
     sorted_ind = np.argsort(np.abs(pls.coef_[:,0]))
- 
     Xc = X[:,sorted_ind]
  
     return(Xc[:,mseminy[0]:],mseminx[0]+1,mseminy[0], sorted_ind)
 
 
-def func(args):
-    i, x_to_permute, n_comp = args
-    np.random.seed(i + x_to_permute*1000)
-
-    # permute x_train for feature x_to_permute
-    perm_idx = np.random.choice(len(X_train), len(X_train), False)
-    x_train_p = X_train.copy()
-    x_train_p[:, x_to_permute] = x_train_p[perm_idx, x_to_permute]
-
-    # bts_est = ExtraTreesClassifier()
-    # bts_est.fit(x_train_p, y_train.ravel())
-    pls = PLSRegression(n_components=n_comp)
-    pls.fit(x_train_p, y_train)
-
-    # permute x_val for feature x_to_permute
-    perm_idx = np.random.choice(len(X_test), len(X_test), False)
-    x_val_p = X_test.copy()
-    x_val_p[:, x_to_permute] = x_val_p[perm_idx, x_to_permute]
-
-    score = pls.score(x_val_p, y_test)
-
-    return score
-
-
-def get_scores_of_permuted_features(X_train, n_comp):
-    scores_permuted = []
-    cpu_count = os.cpu_count()
-
-    for x_to_permute in range(X_train.shape[1]):
-        score = multiprocessing.Pool(cpu_count).map(func, itertools.product(range(99), [x_to_permute], [n_comp] * 50))
-        scores_permuted.append(score)
-    return np.array(scores_permuted).T
-
-
 # --------------------------------------------------
-def simple_pls_cv(X, y, n_comp, transcript, rng=123, trained_model=None):#, permutation=False):
+def simple_pls_cv(X, y, n_comp, transcript, rng=123, trained_model=None):
+    '''
+    Run PLS given the explanatory and response variables, number of\
+         maximum components, and transcript name, and trained model (optional). If model is present, permutations will be run
+
+    Input:
+        - X: explanatory variable (hyperspectral data)
+        - y: response variable (RNA-Seq data)
+        - max_comp: maximum number of allowed components
+        - transcript: name of transcript, string variable
+        - rng: random seed
+        - trained_model: path to trained model; if present, permutations will be run
+    '''
 
     args = get_args()
     if trained_model:
@@ -197,36 +179,10 @@ def simple_pls_cv(X, y, n_comp, transcript, rng=123, trained_model=None):#, perm
             wl = np.arange(350, 2501, 1)
             
             # Define the PLS regression object & fit data
-            pls_temp = PLSRegression(n_components=8)
-            pls_temp.fit(X_perm, y)
+            # pls_temp = PLSRegression(n_components=8)
+            # pls_temp.fit(X_perm, y)
 
-            # # Plot spectra
-            # plt.figure(figsize=(8,9))
-            # with plt.style.context(('ggplot')):
-            #     ax1 = plt.subplot(211)
-            #     plt.plot(wl, X_perm.T)
-            #     plt.ylabel('First derivative spectra')
-
-            #     ax2 = plt.subplot(212, sharex=ax1)
-            #     plt.plot(wl, np.abs(pls_temp.coef_[:,0]))
-            #     plt.xlabel('Wavelength (nm)')
-            #     plt.ylabel('Absolute value of PLS coefficients')
-            #     plt.savefig(os.path.join(permutation_out_dir, f'{transcript}_first_derivative_absolute_value_pls_coeff_permutation_{i}.png'), transparent=True)
-            #     # plt.show()
-            
-            # # Plot spectra with superimpose selected bands
-            # ix = np.in1d(wl.ravel(), wl[sorted_ind][:wav])
-            # fig, ax = plt.subplots(figsize=(8,9))
-            # with plt.style.context(('ggplot')):
-            #     ax.plot(wl, X_perm.T)
-            #     plt.ylabel('First derivative spectra')
-            #     plt.xlabel('Wavelength (nm)')
-                
-            # collection = collections.BrokenBarHCollection.span_where(
-            #     wl, ymin=-1, ymax=1, where=ix == True, facecolor='red', alpha=0.3)
-            # ax.add_collection(collection)
-            # plt.savefig(os.path.join(permutation_out_dir, f'{transcript}_variable_selection_permutation_{str(i).zfill(4)}.png'), transparent=True)
-            
+            # Calculate performance
             Xc = X_perm[:,sorted_ind]
             X_perm = Xc[:,mseminy[0]:]
             result = pls.score(X_perm, y)
@@ -239,6 +195,7 @@ def simple_pls_cv(X, y, n_comp, transcript, rng=123, trained_model=None):#, perm
         res_df = pd.DataFrame(score_list, columns=['score_permutation'])
 
     else:
+
         # Split data into train & test datasets
         X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=rng, test_size=args.test_size)
         
@@ -288,6 +245,15 @@ def simple_pls_cv(X, y, n_comp, transcript, rng=123, trained_model=None):#, perm
 
 # --------------------------------------------------
 def run_variable_selection(df, transcript):
+    '''
+    Run band variable selection, train model, and output model to disk.
+
+    Input: 
+        - df: dataframe containing RNA-Seq and hyperspectral data
+        - transcript: name of transcript, string variable
+    Output: 
+        - Saved models and figures for each transcript
+    '''
 
     # Collect response (RNAseq TPM) and explanatory variables (spectra) for a single transcript
     print(f'Processing transcript: {transcript}')
@@ -360,6 +326,15 @@ def run_variable_selection(df, transcript):
 
 # --------------------------------------------------
 def run_variable_selection_permutation(df, transcript):
+    '''
+    Run permutation on data. 
+
+    Input: 
+        - df: dataframe containing RNA-Seq and hyperspectral data
+        - transcript: name of transcript, string variable
+    Output: 
+        - Saved performance CSV. 
+    '''
 
     # Collect response (RNAseq TPM) and explanatory variables (spectra) for a single transcript
     print(f'Processing transcript with permutation: {transcript}')
@@ -370,7 +345,7 @@ def run_variable_selection_permutation(df, transcript):
     X1 = savgol_filter(X, 11, polyorder = 2, deriv=1)
         
     filename = os.path.join(model_out_dir, '_'.join([transcript, 'model.sav']))
-    res_df, pls = simple_pls_cv(X1, y, ncomp, transcript=transcript, trained_model=filename)#, permutation=False)
+    res_df, pls = simple_pls_cv(X1, y, ncomp, transcript=transcript, trained_model=filename)
     out_file = os.path.join(csv_out_dir, '_'.join([transcript, 'correlation_score_permutation.csv']))
     res_df['transcript'] = transcript
     res_df = res_df.set_index('transcript')
